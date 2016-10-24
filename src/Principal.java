febe4ed37db4ba49d08400a207a46fc607cb81fa
Julio Malliotti <malliottijulio@gmail.com>
2016-10-23 22:32:58 -0200

    Adicionado menu de opcoes e reorganizacao dos metodos.

--- src/Principal.java
+++ src/Principal.java
@@ -1,16 +1,96 @@
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Principal {
 
 	public static void main(String[] args) {
-		try {
-			(new Principal()).executar();
-		} catch (FileNotFoundException e) {
-			System.err.println("Arquivo nao encontrado.");
-			System.err.println(e.getMessage());
-		}
-	}
+        int opcao;
+        Scanner entrada = new Scanner(System.in);
+        
+        do{
+            menu();
+            opcao = entrada.nextInt();
+            
+            switch(opcao){
+            case 0:
+                exit();
+                break;
+                
+            case 1:
+                inclui();
+                break;
+                
+            case 2:
+                altera();
+                break;
+                
+            case 3:
+                exclui();
+                break;
+                
+            case 4:
+                consulta();
+                break;
+            case 5:
+                completa();
+                break;
+                
+            default:
+                System.out.println("Opção inválida.");
+            }
+        } while(opcao != 0);
 
+	}
+	
+    public static void menu(){
+    	System.out.println("\t===============");
+    	System.out.println("\tRotas de Onibus");
+        System.out.println("\t===============");
+        System.out.println("0. Fim");
+        System.out.println("1. Inclui");
+        System.out.println("2. Altera");
+        System.out.println("3. Exclui");
+        System.out.println("4. Consulta");
+        System.out.println("5. Lista Completa");
+        System.out.println("Opcao:");
+    }
+    
+    public static void exit(){
+    	System.out.println();
+    	System.out.println("Sistema encerrado0");
+        System.exit(1);
+    }
+    
+    public static void inclui(){
+    	System.out.println();
+        System.out.println("Você entrou no método Inclui.");
+    }
+    
+    public static void altera(){
+    	System.out.println();
+        System.out.println("Você entrou no método Altera.");
+    }
+    
+    public static void exclui(){
+    	System.out.println();
+        System.out.println("Você entrou no método Exclui.");
+    }
+    
+    public static void consulta(){
+    	System.out.println();
+        System.out.println("Você entrou no método Consulta.");
+    }
+    public static void completa(){
+    	try {	
+    		System.out.println();
+    		(new Principal()).executar();
+    		System.out.println();
+    	} catch (FileNotFoundException e) {
+    		System.err.println("Arquivo nao encontrado.");
+    		System.err.println(e.getMessage());
+    	}
+    }
+    	
 	private void executar() throws FileNotFoundException {
 		validarRotas();
 	}
